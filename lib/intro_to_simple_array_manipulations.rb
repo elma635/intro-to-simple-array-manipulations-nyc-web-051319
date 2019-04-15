140cfef583fa6f037f8ff7cae5cb03076a87adff
Elma Rahman <elmarahman635@gmail.com>
2019-04-15 22:56:39 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,20 +7,15 @@ next_country = "Niger"
 updated_array = countries_in_western_africa.push(next_country)
 
 end
-end
+
 
 def using_unshift(new_neighborhood, updated_array)
-neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-
-new_neighborhood = "Brooklyn Heights"
-
-updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  new_neighborhood.unshift(updated_array)
 
 end
 
 
 def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
 
 deleted_string = great_hits_of_the_nineties.pop
 
@@ -28,57 +23,54 @@ end
 
 
 def pop_with_args (chars_arya_killed)
-chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-chars_arya_killed = chars_in_game_of_thrones.pop(2)
+     chars_arya_killed.pop(2)
 end
 
 
-def shift_with_args(my_favorite_cities)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = using_shift(my_favorite_cities)
+def using_shift(my_favorite_cities)
+  im_so_over_this_city = my_favorite_cities.shift
 
 end
 
+def shift_with_args(brands_removed)
+    brands_removed.shift(2)
 
-def using_concat(more_favs,all_my_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
-  all_my_favs = using_concat(my_favorite_things, more_favs)
+ #brands_removed = arg in .shift
+#returns what it removes
+
+end
+
+def using_concat(more_favs, all_my_favs)
+more_favs.concat(all_my_favs)
+#return would give a concat statement
 end
 
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  another_esoteric_language = "Malbolge"
-  new_array = list_of_esoteric_programming_languages.insert(another_esoteric_language)
+
+list_of_esoteric_programming_languages.insert(4,another_esoteric_language)
+#one element inside of the array
 end
 
 
 def using_uniq(new_array)
-  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-  new_array = using_uniq(captain_planet_and_the_planeteers)
+  new_array.uniq
 end
 
 
 def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-  flat_array = using_flatten(private_colleges_in_newyork)
+  flat_array = private_colleges_in_newyork.flatten
 
 end
 
 
-def using_delete(updated_array,no_offense_steven)
-instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-
-no_offense_steven = "Steven"
-
-no_offense_steven = using_delete(instructors, "Steven")
+def using_delete(updated_array, no_offense_steven)
+updated_array.delete(no_offense_steven)
 end
 
 
-def using_delete_at(deleted_robot, famous_robots)
-famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-
-deleted_robot = famous_robots.delete_at(2)
+def using_delete_at(deleted_robot, number)
+deleted_robot.delete_at(number)
 end
- 
+
+#take what it is, array and number in that case
