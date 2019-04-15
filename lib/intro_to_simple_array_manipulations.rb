dfb1759aa48b721e4353880a6ed842924aed4b66
Elma Rahman <elmarahman635@gmail.com>
2019-04-15 21:56:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,6 @@ end
 
 
 def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
 
 deleted_string = great_hits_of_the_nineties.pop
 
