330751420fdd4f4aca8cb3789b70cc421e462afa
Elma Rahman <elmarahman635@gmail.com>
2019-04-15 22:06:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,8 +44,7 @@ brands_removed = ice_cream_brands.shift(2)
 end
 
 def using_concat(more_favs, all_my_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"
+
   all_my_favs = more_favs.concat(my_favorite_things)
 end
 
